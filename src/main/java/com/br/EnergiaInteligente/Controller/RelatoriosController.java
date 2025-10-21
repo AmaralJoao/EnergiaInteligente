467be4209007e6c0fedc61e0ec3c9da28e0d952b
João Guilherme/win10 <joaoguilherme.amaral05@gmail.com>
2025-10-21 17:27:23 -0300

    Ajustes nas telas

---
+++ src/main/java/com/br/EnergiaInteligente/Controller/RelatoriosController.java
@@ -0,0 +1,32 @@
+package com.br.EnergiaInteligente.Controller;
+
+import com.br.EnergiaInteligente.Dto.Response.LocalizacaoResponseDto;
+import com.br.EnergiaInteligente.Utils.AutenticacaoUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/relatorios")
+public class RelatoriosController {
+
+    @Autowired
+    private AutenticacaoUtils autenticacaoUtils;
+
+    @GetMapping()
+    public String home(Model model) {
+        if (!AutenticacaoUtils.isSessaoValida()) {
+            return "redirect:/auth/login";
+        }
+
+        String codigoPublicoUsuario = autenticacaoUtils.getCodigoPublicoUsuario();
+
+
+        return "pages/home/relatorios/indexRelatorios";
+    }
+
+}
